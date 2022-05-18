8c55b40532c31440c16698888e4031097c277d3a
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-05-18 23:22:16 +0300

    add start files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { elementTemplate } from './js/element-template';
